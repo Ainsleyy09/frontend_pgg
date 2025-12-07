791ed0af8a65d9ad027353ea525513dce277c0d9
Ainsleyy09 <inur44777@gmail.com>
2025-12-07 15:00:10 +0700

    Upload

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-// const url = "https://kelompokfwd9-sibm3.karyakreasi.id";
+// const url = "https://backend-kelompokfwd9-sibm3.karyakreasi.id";
 const url = "http://127.0.0.1:8000";
 
 export const API = axios.create({
