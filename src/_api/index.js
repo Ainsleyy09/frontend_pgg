8a9ae6ff546c9040f232b868cd2ffed564985df2
Ainsleyy09 <inur44777@gmail.com>
2025-12-07 11:25:03 +0700

    Frontend Terbaru

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-// const url = "https://akmal-bc.karyakreasi.id/api";
+// const url = "https://kelompokfwd9-sibm3.karyakreasi.id";
 const url = "http://127.0.0.1:8000";
 
 export const API = axios.create({
     baseURL: `${url}/api`,
-    
+
 })
 
 export const guideImageStorage = `${url}/storage/guides`;
